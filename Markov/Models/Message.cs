af7e17aa09d3b022952906a97bf78fa19ee5718f
Node <Node@DESKTOP-K0I3FLR>
2025-04-06 21:02:09 +0530

    Add project files.

---
+++ Markov/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markov.Models
+{
+    public class Message
+    {
+        public int Sender { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime Time { get; set; } = DateTime.Now;
+    }
+}
